6f34bb971ee7fc51c1f42933382244932d425d53
S-E0K <rqt6238@naver.com>
2024-07-10 13:30:20 +0900

    패킷 어케 해야할까...

--- src/main/java/org/se0k/entity_ai/sound/SoundEffect.java
+++ src/main/java/org/se0k/entity_ai/sound/SoundEffect.java
@@ -4,12 +4,10 @@ import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
-import org.bukkit.Location;
-import org.bukkit.entity.Player;
 
 public class SoundEffect {
 
-    public void soundEffect(Player player, Location location, float volume, float pitch) {
+    public void soundEffect() {
         ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
 
         PacketContainer packetContainer = new PacketContainer(PacketType.Play.Server.NAMED_SOUND_EFFECT);
